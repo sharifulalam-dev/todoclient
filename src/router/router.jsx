fdbdfe3e7a2b27db3463c5dee478fe6652fff28b
sharifulalam-dev <sharifulalam.tex@gmail.com>
2025-02-23 10:22:14 +0600

    update routes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import Dashboard from "../Pages/Dashboard";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+
 import MainLayout from "./../MainLayout/MainLayout";
+import Dashboard from "./../Pages/Dashboard";
+import Home from "./../Pages/Home";
+import Login from "./../Pages/Login";
+import Register from "./../Pages/Register";
 import PrivateRoute from "./../PrivateRoute/PrivateRoute";
 
 const router = createBrowserRouter([
